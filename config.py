985a0f7c6a206d8af9b741186be0d23d9bd656b1
Ryan Kim <106494681+rybread01@users.noreply.github.com>
2022-09-15 20:10:43 -0400

    Add files via upload
    added engine and get session

---
+++ config.py
@@ -0,0 +1,7 @@
+postgresql = {'protocol' : 'postgresql',
+              'username' : 'postgres',
+              'password' : 'passwordlol',
+              'host' : 'localhost',
+              'port' : 5432,
+              'db_name' : 'drug_overdose_db'
+}
