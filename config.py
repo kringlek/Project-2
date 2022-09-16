98d887f83e53ffb0163ee7d930bb8a923f9f3755
kringlek <cooperkk@email.chop.edu>
2022-09-15 23:58:53 -0400

    Final Code.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 postgresql = {'protocol' : 'postgresql',
               'username' : 'postgres',
-              'password' : 'passwordlol',
+              'password' : 'piper77!-amish12!',
               'host' : 'localhost',
-              'port' : 5432,
-              'db_name' : 'drug_overdose_db'
+              'port' : 5433,
+              'db_name' : 'drug_income_db'
 }
